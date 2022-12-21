7cc4e0ae915d1e10cee040b20822e3fb9f7ff6e0
Loïc Faure-Lacroix <lamerstar@gmail.com>
2022-12-20 22:12:59 -0500

    Fixed bug with spec None

--- src/overlaymodule/finders.py
+++ src/overlaymodule/finders.py
@@ -16,20 +16,28 @@ class OverlayFinder(MetaPathFinder):
             self.overlays
         )
 
-    def find_spec(self, fullname, path=None, target=None):
-        module = fullname.split('.')
-
-        if module[0] != self.base_module:
-            return None
-
-
-        module_path = self.base_path / "/".join(module[1:]) / '__init__.py'
+    def get_source_path(self, base_path, module):
+        module_path = base_path / "/".join(module) / '__init__.py'
 
         if not module_path.exists():
             module_path = module_path.parent.parent / f"{module_path.parent.name}.py"
 
             if not module_path.exists():
                 return
+
+        return module_path
+
+
+    def find_spec(self, fullname, path=None, target=None):
+        module = fullname.split('.')
+
+        if module[0] != self.base_module:
+            return None
+
+        module_path = self.get_source_path(self.base_path, module[1:])
+
+        if not module_path:
+            return
 
         spec = importlib.util.spec_from_file_location(
             fullname,
